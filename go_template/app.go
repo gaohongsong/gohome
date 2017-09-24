8ad26f01a2d79d1a9fd85723e48f27a38de59d98
gmaclinuxer <gmaclinuxer@github.com>
2017-09-24 10:43:05 +0800

    feature: go_template preload in init

--- go_template/app.go
+++ go_template/app.go
@@ -5,7 +5,47 @@ import (
 	"log"
 	"os"
 	"fmt"
+	"path/filepath"
+	"io"
+	"github.com/kataras/iris/core/errors"
 )
+
+var templates map[string]*template.Template
+
+// 模板渲染
+func renderTemplate(wr io.Writer, name string, data interface{}) error {
+	tpl, ok := templates[name]
+	if !ok {
+		return errors.New("Template File Not Exist: " + name)
+	}
+	return tpl.ExecuteTemplate(wr, name, data)
+}
+
+// 模板预加载
+func init() {
+	// map初始化，分配内存
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+	// 模板目录
+	templateDir := "./app/"
+	// 视图模板
+	layouts, err := filepath.Glob(filepath.Join(templateDir, "layouts/*.html"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 基础模板
+	widgets, err := filepath.Glob(filepath.Join(templateDir, "widgets/*.html"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// index.html footer.html header.html
+	for _, layout := range layouts {
+		files := append([]string{layout}, widgets...)
+		log.Printf("Init【%s】: %v", layout, files)
+		templates[filepath.Base(layout)] = template.Must(template.ParseFiles(files...))
+	}
+}
 
 func main() {
 
@@ -15,6 +55,7 @@ func main() {
 	//	"app/header.html",
 	//	"app/footer.html",
 	//	"app/content.html"))
+
 	t := template.Must(template.ParseFiles(
 		"app/index.html",
 		"app/header.html",
@@ -27,8 +68,10 @@ func main() {
 	}
 	// 模板相互独立
 	fmt.Println("================================================")
-	t.ExecuteTemplate(os.Stdout, "header", data);fmt.Println()
-	t.ExecuteTemplate(os.Stdout, "content", data);fmt.Println()
+	t.ExecuteTemplate(os.Stdout, "header", data)
+	fmt.Println()
+	t.ExecuteTemplate(os.Stdout, "content", data)
+	fmt.Println()
 	t.ExecuteTemplate(os.Stdout, "footer", data)
 	fmt.Println("================================================")
 
@@ -44,4 +87,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("\n================================= 预加载")
+	err = renderTemplate(os.Stdout, "index.html", map[string]string{
+		"Title": "Sleep?",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
