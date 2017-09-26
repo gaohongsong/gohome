246b09cbf90e906204a02b596a8d447a661f9788
gmaclinuxer <gmaclinuxer@github.com>
2017-09-26 16:30:51 +0800

    feature: go_fasthttp

--- go_fasthttp/main.go
+++ go_fasthttp/main.go
@@ -1,58 +1,27 @@
-package go_fasthttp
+package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 
 	"github.com/valyala/fasthttp"
+	"github.com/buaazp/fasthttprouter"
+	"encoding/json"
 )
 
-var (
-	addr     = flag.String("addr", ":8080", "TCP address to listen to")
-	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
-)
-
-func main() {
-	flag.Parse()
-
-	h := requestHandler
-	if *compress {
-		h = fasthttp.CompressHandler(h)
-	}
-
-	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
-		log.Fatalf("Error in ListenAndServe: %s", err)
-	}
+func indexHandle(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("application/json")
+	b, _ := json.Marshal(map[string]interface{}{
+		"name": "miya",
+		"age": 1,
+	})
+	ctx.SetBody(b)
 }
 
-func requestHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
 
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	form, _ := ctx.MultipartForm()
-	fmt.Fprintf(ctx,"MultipartForm: %q\n", form)
-	fmt.Fprintf(ctx, "PostArgs is %q\n", ctx.PostArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
-
-	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
-
-	ctx.SetContentType("text/plain; charset=utf8")
-
-	// Set arbitrary headers
-	ctx.Response.Header.Set("X-My-Header", "my-header-value")
-
-	// Set cookies
-	var c fasthttp.Cookie
-	c.SetKey("cookie-name")
-	c.SetValue("cookie-value")
-	ctx.Response.Header.SetCookie(&c)
+func main() {
+	router := fasthttprouter.New()
+	router.GET("/", indexHandle)
+	if err := fasthttp.ListenAndServe(":8080", router.Handler); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
 }
